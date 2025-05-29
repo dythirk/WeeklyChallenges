fbc3f8729688b1c69b3d83838d330d1dcf83586e
Thai Vo <tahrbo@gmail.com>
2025-05-29 13:01:55 -0500

    Almost done

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -13,37 +13,64 @@ namespace ChallengesWithTestsMark8
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
+            //if (numbers == null)
+            //    return false;
+            //else
+            //{
+            //    int total = 0;
+            //    foreach (int n in numbers)
+            //    {
+            //        if (numbers(n) % 2 == 0)
+            //            total += numbers[n];
+            //    }
+            //}
             throw new NotImplementedException();
+
+
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            if (password.Any(char.IsUpper) & password.Any(char.IsLower) & password.Any(char.IsDigit))
+                return true;
+            else
+                return false;
+
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val[0];
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val[val.Length-1];
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if (divisor == 0)
+                return 0;
+            else
+                return dividend / divisor;
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            return nums[nums.Length - 1] - nums[0];
+        
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            int[] odd = {};
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 2 != 0)
+                    odd = odd.Concat(new int[] { i }).ToArray();
+            }
+            return odd;
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
